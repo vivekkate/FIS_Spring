002f439a355acaea5241aef47ce012f77a44cafc
vivekkate <vivekkate999@gmail.com>
2021-08-09 17:37:31 +0530

    added incorporate logging

--- spring-learn/spring-learn/src/main/java/com/fis/springlearn/SpringLearnApplication.java
+++ spring-learn/spring-learn/src/main/java/com/fis/springlearn/SpringLearnApplication.java
@@ -4,6 +4,10 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,9 +15,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class SpringLearnApplication {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLearnApplication.class, args);
+		LOGGER.info("Start");
 		displayDate();
 	}
 	
@@ -24,9 +31,12 @@ public class SpringLearnApplication {
 		 try {
 			Date parseDate = format.parse("31/12/2018");
 			System.out.println(parseDate);
+			LOGGER.debug(parseDate.toString());
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
+		 
+		 LOGGER.info("End");
 	}
 
 }
