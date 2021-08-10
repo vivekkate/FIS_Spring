a3ae6fc3ce0daad57daa2ea74a251010e2d27771
vivekkate <vivekkate999@gmail.com>
2021-08-10 11:30:16 +0530

    loaded country in spring project

---
+++ spring-learn/spring-learn/src/main/java/com/fis/springlearn/Country.java
@@ -0,0 +1,38 @@
+package com.fis.springlearn;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Country {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(Country.class);
+
+	
+	String code;
+	String name;
+	
+	public Country() {
+		LOGGER.debug("Inside Country Constructor");
+	}
+	
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Country [code=" + code + ", name=" + name + "]";
+	}
+	
+	
+	
+
+}
--- spring-learn/spring-learn/src/main/java/com/fis/springlearn/SpringLearnApplication.java
+++ spring-learn/spring-learn/src/main/java/com/fis/springlearn/SpringLearnApplication.java
@@ -7,7 +7,6 @@ import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -15,28 +14,36 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class SpringLearnApplication {
-	
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLearnApplication.class, args);
 		LOGGER.info("Start");
 		displayDate();
+		displayCountry();
 	}
-	
-	
-	static void displayDate()  {
-		 ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
-		 SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
-		 try {
+
+	static void displayDate() {
+		ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
+		SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
+		try {
 			Date parseDate = format.parse("31/12/2018");
 			System.out.println(parseDate);
 			LOGGER.debug(parseDate.toString());
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-		 
-		 LOGGER.info("End");
+
+		LOGGER.info("End");
+	}
+	
+	
+	static void displayCountry() {
+		ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+		Country country = (Country) context.getBean("country", Country.class);
+		LOGGER.debug("Country : {}", country.toString());
+
 	}
 
 }
