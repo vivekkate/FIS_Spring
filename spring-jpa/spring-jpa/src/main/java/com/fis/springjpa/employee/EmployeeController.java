fa31b35372bd88b381dd5e5947909cd217b18aed
vivekkate <vivekkate999@gmail.com>
2021-08-12 12:34:25 +0530

    added CRUD operations for employee

--- spring-jpa/spring-jpa/src/main/java/com/fis/springjpa/employee/EmployeeController.java
+++ spring-jpa/spring-jpa/src/main/java/com/fis/springjpa/employee/EmployeeController.java
@@ -1,39 +1,70 @@
 package com.fis.springjpa.employee;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.fis.springjpa.User;
-
-@Controller
-@RequestMapping(path="/employee")
+@RestController 
+@RequestMapping(path = "/employee") 
 public class EmployeeController {
+	@Autowired
+	private EmployeeRepository employeeRepository;
 
-@Autowired
-private EmployeeRepository employeeRepository;
-@PostMapping(path="/add")
-public @ResponseBody String addEmployee (@RequestParam int id
-	      , @RequestParam String name) {
-	    // @ResponseBody means the returned String is the response, not a view name
-	    // @RequestParam means it is a parameter from the GET or POST request
+	@PostMapping
+	public @ResponseBody String addNewUser(@RequestBody Employee employee) {
+		employeeRepository.save(employee);
+		return "Saved";
+	}
 
-	    Employee n = new Employee();
-	    n.setId(id);
-	    n.setName(name);
-	    employeeRepository.save(n);
-	    return "Saved";
-	  }
+	@GetMapping
+	public @ResponseBody Iterable<Employee> getAllUsers() {
+		
+		return employeeRepository.findAll();
+	}
 
+	@PutMapping
+	public String updateEmployee(@RequestBody Employee employee) {
 
-@GetMapping(path="/all")
-public @ResponseBody Iterable<Employee> getAllUsers() {
-  // This returns a JSON or XML with the users
-  return employeeRepository.findAll();
-}
+		Optional<Employee> emp = employeeRepository.findById(employee.getId());
+		if (emp.isPresent()) {
+			employeeRepository.save(employee);
+			return "Updated";
+		} else {
+			return "Employee not found";
+		}
 
+	}
+
+	@DeleteMapping
+	public String Employee(@RequestParam int id) {
+
+		Optional<Employee> emp = employeeRepository.findById(id);
+		if (emp.isPresent()) {
+			employeeRepository.delete(emp.get());
+			return "Deleted";
+		} else {
+			return "Employee not found";
+		}
+
+	}
+	
+	@GetMapping("/{id}")
+	public Object getAnEmployee(@PathVariable int id) {
+		Optional<Employee> emp = employeeRepository.findById(id);
+		if (emp.isPresent()) {
+			return emp.get();
+		} else {
+			return "Employee Not Found!!";
+		}
+	}
 }
