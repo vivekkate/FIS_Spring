669a85c104ac728a4307b12737d114b7ca3199d8
vivekkate <vivekkate999@gmail.com>
2021-08-17 09:17:39 +0530

    added spring testing

---
+++ spring-testing/src/main/java/com/fis/springtesting/GreetingService.java
@@ -0,0 +1,12 @@
+package com.fis.springtesting;
+
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+	public String greet() {
+		return "Hello, World";
+	}
+}
---
+++ spring-testing/src/test/java/com/fis/springtesting/WebLayerTest.java
@@ -0,0 +1,26 @@
+package com.fis.springtesting;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+@WebMvcTest
+public class WebLayerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void shouldReturnDefaultMessage() throws Exception {
+		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+				.andExpect((ResultMatcher) content().string(containsString("Hello, World")));
+	}
+}
