8c0ab4aeb7614b8e8476eb2c2eb64366f24b3e8f
vivekkate <vivekkate999@gmail.com>
2021-08-17 09:21:48 +0530

    integrated JPA and security

--- spring-security/src/main/java/com/fis/springsecurity/SpringSecurityApplication.java
+++ spring-security/src/main/java/com/fis/springsecurity/SpringSecurityApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringSecurityApplication {
 
+	//
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityApplication.class, args);
 	}
